9129474ff7a3b5661b1dd3f760089f4c3648ecf1
xulishuang <xulishuang_bit@126.com>
2017-07-25 16:54:51 +0800

    script.models.BangMaiChe增加releaseGeneralTopic和releaseVoteTopic方法

--- src/models/BangMaiChe.java
+++ src/models/BangMaiChe.java
@@ -1,5 +1,7 @@
 package models;
 
+import org.openqa.selenium.By;
+
 import common.Common;
 import io.appium.java_client.ios.IOSDriver;
 
@@ -50,6 +52,55 @@ public class BangMaiChe {
 		Common.touchText(driver,"添加车型");
 		Common.touchText(driver,"奥迪");
 		Common.touchText(driver,"奥迪A6L");
+	}
+	
+	/** 
+	 * @function “帮买车—发帖”发布普通话题
+	 * @param driver
+	 * @author xulishuang
+	 * @time 2017-7-25
+	 * 
+	 */
+	@SuppressWarnings("rawtypes")
+	public static void releaseGeneralTopic(IOSDriver driver) throws InterruptedException{
+		//点击进入帮买车
+		Common.touchText(driver,"帮买车");
+		//点击最新
+		Common.touchText(driver,"最新");
+		//点击发帖
+		Common.touchText(driver,"发帖");
+		//点击话题icon
+		Common.touchText(driver, "话题");
+		//输入内容
+		Common.sendText(driver, By.xpath("//UIAApplication[1]/UIAWindow[1]/UIAScrollView[1]/UIATextView[1]"), "奥迪Q5空间大吗");
+		//点击提交
+		Common.touchText(driver, "提交");
+	}
+	
+	/** 
+	 * @function “帮买车—发帖”发布投票话题
+	 * @param driver
+	 * @author xulishuang
+	 * @time 2017-7-25
+	 * 
+	 */
+	@SuppressWarnings("rawtypes")
+	public static void releaseVoteTopic(IOSDriver driver) throws InterruptedException{
+		//点击进入帮买车
+		Common.touchText(driver,"帮买车");
+		//点击最新
+		Common.touchText(driver,"最新");
+		//点击发帖
+		Common.touchText(driver,"发帖");
+		//点击投票icon
+		Common.touchText(driver, "投票");
+		//添加两种车型
+		BangMaiChe.addModels(driver);
+	    //选择在意方面：
+		Common.touchXpath(driver, "//UIAApplication[1]/UIAWindow[1]/UIATableView[1]/UIATableCell[4]/UIAButton[1]");
+		Common.touchXpath(driver, "//UIAApplication[1]/UIAWindow[1]/UIATableView[1]/UIATableCell[4]/UIAButton[2]");
+		//点击发布
+		Common.touchText(driver,"提交");
 	}
 
 }
