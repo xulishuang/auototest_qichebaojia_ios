a4f7914b59d75572d334c0246e02862dd7e14499
xulishuang <xulishuang_bit@126.com>
2017-07-26 17:06:45 +0800

    WoDe.java增加setCommentsEmpty方法

--- src/models/WoDe.java
+++ src/models/WoDe.java
@@ -52,6 +52,26 @@ public class WoDe {
 		Common.touchText(driver, "old navBackButton nor");
 	}
 	
+	/**
+	 * @function 清空 我的社区-评论 已发评论
+	 * @param driver
+	 * @throws InterruptedException
+	 * @author xulishuang
+	 * @date 2017-7-26
+	 */
+	@SuppressWarnings("rawtypes")
+	public static void setCommentsEmpty(IOSDriver driver) throws InterruptedException{
+		//清空评论
+		Common.touchText(driver, "我的");
+		Common.touchText(driver, "我的社区");
+		Common.touchXpath(driver, "//UIAApplication[1]/UIAWindow[1]/UIATableView[1]/UIAButton[2]");//点击评论
+		while(Common.checkTextExist(driver, "删除")){
+			Common.touchText(driver, "删除");
+			Common.touchText(driver, "确定");
+			}
+		Common.multiBack(driver, 2);
+	}
+	
 	/** 
 	 * @function 清空“车型收藏-车型”中已收藏车型
 	 * @param driver
@@ -61,7 +81,7 @@ public class WoDe {
 	 */ 
 	@SuppressWarnings("rawtypes")
 	public static void setModelsCollectionEmpty(IOSDriver driver) throws InterruptedException{
-		//进入我的社区-话题
+		//进入我的-车型收藏
 		Common.touchText(driver,"我的");
 		Common.touchText(driver, "车型收藏");
 		//清空车型收藏
@@ -69,6 +89,76 @@ public class WoDe {
 			Common.touchXpath(driver, "//UIAApplication[1]/UIAWindow[1]/UIAScrollView[1]/UIATableView[1]/UIATableCell[1]");
 			Common.touchText(driver, "fav nor");
 			Common.back(driver);
+		}
+		//返回首页
+		Common.back(driver);
+	}
+	
+	/** 
+	 * @function 清空“车型收藏-车款”中已收藏车款
+	 * @param driver
+	 * @author xulishuang
+	 * @time 2017-6-1
+	 * 
+	 */ 
+	@SuppressWarnings("rawtypes")
+	public static void setCarCollectionEmpty(IOSDriver driver) throws InterruptedException{
+		//进入我的-车型收藏
+		Common.touchText(driver,"我的");
+		Common.touchText(driver, "车型收藏");
+		Common.touchText(driver,"车款");
+		//清空已收藏车款
+		while (Common.checkXpathExist(driver,"//UIAApplication[1]/UIAWindow[1]/UIAScrollView[1]/UIATableView[1]/UIATableCell[1]")){
+			Common.touchXpath(driver, "//UIAApplication[1]/UIAWindow[1]/UIAScrollView[1]/UIATableView[1]/UIATableCell[1]");
+			Common.touchText(driver, "fav nor");
+			Common.back(driver);
+		}
+		//返回首页
+		Common.back(driver);
+	}
+	
+	/** 
+	 * @function 清空“车型收藏-二手车”中已收藏车款
+	 * @param driver
+	 * @author xulishuang
+	 * @time 2017-6-1
+	 * 
+	 */ 
+	@SuppressWarnings("rawtypes")
+	public static void setUsedCarCollectionEmpty(IOSDriver driver) throws InterruptedException{
+		//进入我的-车型收藏
+		Common.touchText(driver,"我的");
+		Common.touchText(driver, "车型收藏");
+		Common.touchText(driver,"二手车");
+		//清空已收藏车款
+		while (Common.checkXpathExist(driver,"//UIAApplication[1]/UIAWindow[1]/UIAScrollView[1]/UIATableView[1]/UIATableCell[1]")){
+			Common.touchXpath(driver, "//UIAApplication[1]/UIAWindow[1]/UIAScrollView[1]/UIATableView[1]/UIATableCell[1]");
+			Common.touchText(driver, "fav nor");
+			Common.back(driver);
+		}
+		//返回首页
+		Common.back(driver);
+	}
+	
+	/** 
+	 * @function 清空“车型收藏-经销商”中已收藏经销商
+	 * @param driver
+	 * @author xulishuang
+	 * @time 2017-6-7
+	 * 
+	 */ 
+	@SuppressWarnings("rawtypes")
+	public static void setDealersCollectionEmpty(IOSDriver driver) throws InterruptedException{
+		//进入我的-车型收藏
+		Common.touchText(driver,"我的");
+		Common.touchText(driver, "车型收藏");
+		Common.touchText(driver,"经销商");
+		//清空已收藏经销商
+		while (Common.checkXpathExist(driver,"//UIAApplication[1]/UIAWindow[1]/UIAScrollView[1]/UIATableView[1]/UIATableCell[1]")){
+			Common.touchTopRight(driver); //点击编辑
+			Common.touchXpath(driver, "//UIAApplication[1]/UIAWindow[1]/UIAScrollView[1]/UIATableView[1]/UIATableCell[1]/UIAButton[1]");
+			Common.touchXpath(driver, "//UIAApplication[1]/UIAWindow[1]/UIAScrollView[1]/UIATableView[1]/UIATableCell[1]/UIAButton[2]");
+			Common.touchTopRight(driver); //点击完成
 		}
 		//返回首页
 		Common.back(driver);
@@ -96,70 +186,44 @@ public class WoDe {
 	}
 	
 	/** 
-	 * @function 清空“车型收藏-车款”中已收藏车款
+	 * @function 清空“浏览记录-经销商”中已浏览经销商
 	 * @param driver
 	 * @author xulishuang
 	 * @time 2017-6-1
 	 * 
 	 */ 
 	@SuppressWarnings("rawtypes")
-	public static void setCarCollectionEmpty(IOSDriver driver) throws InterruptedException{
-		//进入我的社区-话题
+	public static void setDealersBrowseEmpty(IOSDriver driver) throws InterruptedException{
+		//进入浏览记录
 		Common.touchText(driver,"我的");
-		Common.touchText(driver, "车型收藏");
-		Common.touchText(driver,"车款");
-		//清空已发话题
-		while (Common.checkXpathExist(driver,"//UIAApplication[1]/UIAWindow[1]/UIAScrollView[1]/UIATableView[1]/UIATableCell[1]")){
-			Common.touchXpath(driver, "//UIAApplication[1]/UIAWindow[1]/UIAScrollView[1]/UIATableView[1]/UIATableCell[1]");
-			Common.touchText(driver, "fav nor");
-			Common.back(driver);
+		Common.touchText(driver, "浏览记录");
+		Common.touchText(driver, "经销商");
+		//清空浏览记录
+		if (Common.checkXpathExist(driver,"//UIAApplication[1]/UIAWindow[1]/UIAScrollView[1]/UIATableView[1]/UIATableCell[1]")){
+			Common.touchTopRight(driver);
+			Common.touchText(driver,"删除");
 		}
 		//返回首页
 		Common.back(driver);
 	}
 	
 	/** 
-	 * @function 清空“车型收藏-二手车”中已收藏车款
+	 * @function 清空“浏览记录-降价”中已降价车型
 	 * @param driver
 	 * @author xulishuang
 	 * @time 2017-6-1
 	 * 
 	 */ 
 	@SuppressWarnings("rawtypes")
-	public static void setUsedCarCollectionEmpty(IOSDriver driver) throws InterruptedException{
-		//进入我的社区-话题
+	public static void setCutsBrowseEmpty(IOSDriver driver) throws InterruptedException{
+		//进入浏览记录
 		Common.touchText(driver,"我的");
-		Common.touchText(driver, "车型收藏");
-		Common.touchText(driver,"二手车");
-		//清空已发话题
-		while (Common.checkXpathExist(driver,"//UIAApplication[1]/UIAWindow[1]/UIAScrollView[1]/UIATableView[1]/UIATableCell[1]")){
-			Common.touchXpath(driver, "//UIAApplication[1]/UIAWindow[1]/UIAScrollView[1]/UIATableView[1]/UIATableCell[1]");
-			Common.touchText(driver, "fav nor");
-			Common.back(driver);
-		}
-		//返回首页
-		Common.back(driver);
-	}
-	
-	/** 
-	 * @function 清空“车型收藏-经销商”中已收藏经销商
-	 * @param driver
-	 * @author xulishuang
-	 * @time 2017-6-7
-	 * 
-	 */ 
-	@SuppressWarnings("rawtypes")
-	public static void setDealersCollectionEmpty(IOSDriver driver) throws InterruptedException{
-		//进入我的社区-话题
-		Common.touchText(driver,"我的");
-		Common.touchText(driver, "车型收藏");
-		Common.touchText(driver,"经销商");
-		//清空已发话题
-		while (Common.checkXpathExist(driver,"//UIAApplication[1]/UIAWindow[1]/UIAScrollView[1]/UIATableView[1]/UIATableCell[1]")){
-			Common.touchTopRight(driver); //点击编辑
-			Common.touchXpath(driver, "//UIAApplication[1]/UIAWindow[1]/UIAScrollView[1]/UIATableView[1]/UIATableCell[1]/UIAButton[1]");
-			Common.touchXpath(driver, "//UIAApplication[1]/UIAWindow[1]/UIAScrollView[1]/UIATableView[1]/UIATableCell[1]/UIAButton[2]");
-			Common.touchTopRight(driver); //点击完成
+		Common.touchText(driver, "浏览记录");
+		Common.touchText(driver, "降价");
+		//清空浏览记录
+		if (Common.checkXpathExist(driver,"//UIAApplication[1]/UIAWindow[1]/UIAScrollView[1]/UIATableView[1]/UIATableCell[1]")){
+			Common.touchTopRight(driver);
+			Common.touchText(driver, "删除");
 		}
 		//返回首页
 		Common.back(driver);
