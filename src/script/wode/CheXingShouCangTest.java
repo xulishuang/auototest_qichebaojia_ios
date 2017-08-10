45850b1399d92dd02a17b4aa180b95fede16eeb7
xulishuang <xulishuang_bit@126.com>
2017-08-10 15:50:19 +0800

    修改用例MyCommunityTest.java 增加用例CheXingShouCangTest.java

---
+++ src/script/wode/CheXingShouCangTest.java
@@ -0,0 +1,247 @@
+package script.wode;
+
+import org.testng.Assert;
+import org.openqa.selenium.By;
+import org.testng.annotations.*;
+import script.AppTestSuite;
+import common.*;
+import models.ErShouChe;
+import models.WoDe;
+import models.XinChe;
+
+public class CheXingShouCangTest extends AppTestSuite{
+	
+	/**
+	 * @throws InterruptedException 
+	 * @function 车型-添加车型
+	 */
+	@Test(priority = 1)
+	public void wode_chexingshoucang_testcase1() throws InterruptedException {
+		//清空车型收藏
+		WoDe.setModelsCollectionEmpty(driver);
+		//进入我的-车型收藏-添加车型
+		Common.touchText(driver, "车型收藏");
+		Common.touchText(driver, "添加车型");
+		Common.touchText(driver, "奥迪");
+		Common.touchText(driver, "奥迪Q5");
+		//检查车型收藏添加车型成功
+		Assert.assertTrue(Common.checkTextExist(driver, "车型收藏")&&Common.checkTextExist(driver, "奥迪Q5"), "检查车型收藏添加车型成功");
+        //返回首页
+		Common.back(driver);
+	}
+	
+	/**
+	 * @throws InterruptedException 
+	 * @function 车型-查看车型
+	 * @precondition 接续testcase1
+	 */
+	@Test(priority = 2)
+	public void wode_chexingshoucang_testcase2() throws InterruptedException {
+		//进入车型收藏
+		Common.touchText(driver,"我的");
+		Common.touchText(driver, "车型收藏");
+		//点击车型数据
+		Common.touchXpath(driver,"//UIAApplication[1]/UIAWindow[1]/UIAScrollView[1]/UIATableView[1]/UIATableCell[1]");
+		//检查进入车型综述页面
+		Assert.assertTrue(Common.checkIsSelected(driver, By.name("综述")),"检查进入车型综述页面");
+		//返回首页
+		Common.multiBack(driver,2);
+	}
+	
+	/**
+	 * @function 车型-删除车型
+	 * @precondition 接续testcase1
+	 */
+	@Test(priority = 3)
+	public void wode_chexingshoucang_testcase3() {
+		//进入车型收藏
+		Common.touchText(driver,"我的");
+		Common.touchText(driver, "车型收藏");
+		//删除车型
+		Common.touchTopRight(driver);//点击编辑
+		Common.touchXpath(driver, "//UIAApplication[1]/UIAWindow[1]/UIAScrollView[1]/UIATableView[1]/UIATableCell[1]/UIAButton[1]");//点击-号
+		Common.touchText(driver, "删除");//点击删除
+		//检查车型删除成功
+		Assert.assertTrue(Common.checkTextExist(driver, "暂无收藏相关车型")&&Common.checkTextExist(driver, "添加车型"), "成功返回到车型收藏页，删除成功！");
+        //返回首页
+		Common.back(driver);
+	}
+	
+	/**
+	 * @function 车款-添加车款
+	 * @throws Exception
+	 */
+	@Test(priority = 4)
+	public void wode_chexingshoucang_testcase4() throws Exception{
+		//清空车款
+		WoDe.setCarCollectionEmpty(driver);
+		//添加车款收藏
+		XinChe.collectCars(driver);
+		//进入车型收藏-车款列表查看
+		Common.touchText(driver, "我的");
+		Common.touchText(driver, "车型收藏");
+		Common.touchText(driver, "车款");
+		//检查车款收藏成功
+		Assert.assertTrue(Common.checkXpathExist(driver, "//UIAApplication[1]/UIAWindow[1]/UIAScrollView[1]/UIATableView[1]/UIATableCell[1]"), "检查车款收藏成功");
+		//返回首页
+		Common.back(driver);
+	}
+	
+	/**
+	 * @throws InterruptedException 
+	 * @function 车款-查看车款
+	 * 
+	 */
+	@Test(priority = 5)
+    public void wode_chexingshoucang_testcase5() throws InterruptedException {
+		//进入车型收藏-车款列表查看
+		Common.touchText(driver, "我的");
+		Common.touchText(driver, "车型收藏");
+		Common.touchText(driver, "车款");	
+		//点击车款数据
+		Common.touchXpath(driver, "//UIAApplication[1]/UIAWindow[1]/UIAScrollView[1]/UIATableView[1]/UIATableCell[1]");
+		//检查进入车款最新报价页面
+		Assert.assertTrue(Common.checkTextExist(driver, "dealer paixu nor"), "检查进入车款最新报价页面");
+		//返回首页
+		Common.multiBack(driver, 2);
+	}
+	
+	/**
+	 * @function 车款-删除车款
+	 */
+	@Test(priority = 6)
+    public void wode_chexingshoucang_testcase6() {
+		//进入我的-车型收藏-车款
+		Common.touchText(driver, "我的");
+		Common.touchText(driver, "车型收藏");
+		Common.touchText(driver, "车款");
+		//删除车款
+		Common.touchTopRight(driver);//点击编辑
+		Common.touchXpath(driver, "//UIAApplication[1]/UIAWindow[1]/UIAScrollView[1]/UIATableView[1]/UIATableCell[1]/UIAButton[1]");//点击-号
+		Common.touchText(driver, "删除");//点击删除	
+		//检查删除车款成功
+		Assert.assertTrue(Common.checkTextExist(driver, "暂无收藏相关车款"), "检查删除车款成功");
+		//返回首页
+		Common.back(driver);
+	}
+	
+	/**
+	 * @function 经销商-添加经销商
+	 * @throws Exception
+	 */
+	@Test(priority = 7)
+	public void wode_chexingshoucang_testcase7() throws Exception{
+		//清空经销商
+		WoDe.setDealersCollectionEmpty(driver);
+		//添加经销商收藏
+		XinChe.collectDealers(driver);
+		//进入我的-车型收藏-经销商列表查看
+		Common.touchText(driver, "我的");
+		Common.touchText(driver, "车型收藏");
+		Common.touchText(driver, "经销商");
+		//检查经销商收藏成功
+		Assert.assertTrue(Common.checkXpathExist(driver, "//UIAApplication[1]/UIAWindow[1]/UIAScrollView[1]/UIATableView[1]/UIATableCell[1]"), "检查经销商收藏成功");
+		//返回首页
+		Common.back(driver);
+	}
+	
+	/**
+	 * @throws InterruptedException 
+	 * @function 经销商-查看经销商数据
+	 * @precondition 接续testcase7
+	 */
+	@Test(priority = 8)
+	public void wode_chexingshoucang_testcase8() throws InterruptedException{
+		//进入我的-车型收藏-经销商
+		Common.touchText(driver, "我的");
+		Common.touchText(driver, "车型收藏");
+		Common.touchText(driver, "经销商");
+		//点击经销商数据
+		Common.touchXpath(driver, "//UIAApplication[1]/UIAWindow[1]/UIAScrollView[1]/UIATableView[1]/UIATableCell[1]");
+		//检查进入经销商详情页面
+		Assert.assertTrue(Common.checkTextExist(driver, "促销信息"), "检查进入经销商详情页面");
+		//返回首页
+		Common.multiBack(driver, 2);
+	}
+	
+	/**
+	 * @function 二手车-删除经销商数据
+	 * @precondition 接续testcase7
+	 */
+	@Test(priority = 9)
+	public void wode_chexingshoucang_testcase9(){
+		//进入我的-车型收藏-经销商
+		Common.touchText(driver, "我的");
+		Common.touchText(driver, "车型收藏");
+		Common.touchText(driver, "经销商");
+		//删除经销商收藏
+		Common.touchTopRight(driver);//点击编辑
+		Common.touchXpath(driver, "//UIAApplication[1]/UIAWindow[1]/UIAScrollView[1]/UIATableView[1]/UIATableCell[1]/UIAButton[1]");//点击-号
+		Common.touchText(driver, "删除");//点击删除	
+		//检查删除经销商成功
+		Assert.assertTrue(Common.checkTextExist(driver, "暂无收藏相关经销商"), "检查删除经销商成功");
+		//返回首页
+		Common.back(driver);
+	}
+	
+	/**
+	 * @throws InterruptedException 
+	 * @function 二手车-添加二手车
+	 */
+	@Test(priority = 10)
+	public void wode_chexingshoucang_testcase10() throws InterruptedException{
+		//清空二手车
+		WoDe.setUsedCarCollectionEmpty(driver);
+		//添加二手车收藏
+		ErShouChe.collectUsedCar(driver);
+		//查看我的-车型收藏-二手车列表
+		Common.touchText(driver, "我的");
+		Common.touchText(driver, "车型收藏");
+		Common.touchText(driver, "二手车");
+		//检查二手车收藏成功
+		Assert.assertTrue(Common.checkXpathExist(driver,"//UIAApplication[1]/UIAWindow[1]/UIAScrollView[1]/UIATableView[1]/UIATableCell[1]"),"检查二手车收藏成功");
+		//返回首页	
+		Common.back(driver);
+	}
+		
+	/**
+	 * @function 二手车-查看二手车数据
+	 * @throws InterruptedException 
+	 */
+	@Test(priority = 11)
+	public void wode_chexingshoucang_testcase11() throws InterruptedException{
+		//查看我的-车型收藏-二手车列表
+		Common.touchText(driver, "我的");
+		Common.touchText(driver, "车型收藏");
+		Common.touchText(driver, "二手车");
+		//检查二手车收藏成功
+		Common.touchXpath(driver,"//UIAApplication[1]/UIAWindow[1]/UIAScrollView[1]/UIATableView[1]/UIATableCell[1]");
+		//检查进入车源详情页面
+		Assert.assertTrue(Common.checkTextExist(driver, "车源详情"), "检查进入车源详情页面");
+		//返回首页
+		Common.multiBack(driver,2);
+	}
+		
+	/**
+	 * @function 二手车-删除二手车
+	 */
+	@Test(priority = 12)
+	public void wode_chexingshoucang_testcase12(){
+		//进入我的-车型收藏-经销商
+		Common.touchText(driver, "我的");
+		Common.touchText(driver, "车型收藏");
+		Common.touchText(driver, "二手车");
+		//删除二手车收藏
+		Common.touchTopRight(driver);//点击编辑
+		Common.touchXpath(driver, "//UIAApplication[1]/UIAWindow[1]/UIAScrollView[1]/UIATableView[1]/UIATableCell[1]/UIAButton[1]");//点击-号
+		Common.touchText(driver, "删除");//点击删除	
+		//检查删除二手车成功
+		Assert.assertTrue(Common.checkTextExist(driver, "暂无收藏相关二手车"), "检查删除二手车成功");
+		//返回首页
+		Common.back(driver);
+		}
+			
+}
+	
+
+	
--- src/script/wode/MyCommunityTest.java
+++ src/script/wode/MyCommunityTest.java
@@ -13,316 +13,316 @@ public class MyCommunityTest extends AppTestSuite{
      * @function 我的话题列表-添加话题
      * @throws Exception
      */
-//	@Test(priority = 1)
-//	public void wode_mycommunity_testcase1() throws Exception{
-//		//清空我的话题列表
-//		WoDe.setTopicEmpty(driver);
-//		//发布话题
-//		BangMaiChe.releaseGeneralTopic(driver);
-//		//进入我的话题列表页
-//		WoDe.enterTopicPage(driver);
-//		//检查话题添加成功
-//		Assert.assertTrue(Common.checkTextNotExist(driver,"您还没有发表过话题"),"检查话题发布成功");
-//		//返回首页
-//		Common.multiBack(driver, 2);
-//	}
-//	
-//	/**
-//	 * @function 我的话题列表-话题跳转
-//	 * @precondition 接续testcase1
-//	 * @throws Exception
-//	 */
-//	@Test(priority = 2)
-//	public void wode_mycommunity_testcase2() throws Exception{
-//		//进入我的-我的社区-话题
-//		WoDe.enterTopicPage(driver);
-//		//点击话题数据
-//		Common.touchXpath(driver, "//UIAApplication[1]/UIAWindow[1]/UIATableView[1]/UIATableCell[1]");
-//		//检查进入话题详情页
-//		Assert.assertTrue(Common.checkTextExist(driver, "话题详情"), "成功进入话题详情页");
-//		//返回首页
-//		Common.multiBack(driver, 3);
-//	}
-//	
-//	/**
-//	 * @function 我的话题列表-点赞
-//	 * @preconditon 接续testcase1
-//	 * @throws Exception
-//	 */
-//	@Test(priority = 3)
-//	public void wode_mycommunity_testcase3() throws Exception{
-//		//进入我的-我的社区-话题
-//		WoDe.enterTopicPage(driver);
-//		//点赞
-//		Common.touchXpath(driver, "//UIAApplication[1]/UIAWindow[1]/UIATableView[1]/UIATableCell[1]/UIAButton[3]");
-//		//检查点赞成功
-//		  
-//		//返回首页
-//		Common.multiBack(driver, 2);
-//	}
-//	
-//	/**
-//	 * @function 我的话题列表-评论
-//	 * @precondion 接续testcase1
-//	 * @throws Exception
-//	 */
-//	@Test(priority = 4)
-//	public void wode_mycommunity_testcase4() throws Exception{
-//		//进入我的-我的社区-话题
-//		WoDe.enterTopicPage(driver);
-//		//进行评论
-//		Common.touchText(driver, "topic btn action 2 nor");//点击评论
-//		Common.sendText(driver, By.xpath("//UIAApplication[1]/UIAWindow[1]/UIATextView[1]"), "嗯嗯");//输入内容
-//		Common.touchText(driver, "引用话题");//点击引用话题
-//		Common.touchText(driver,"nav right close");//关闭
-//		Common.touchXpath(driver, "//UIAApplication[1]/UIAWindow[1]/UIAButton[4]");//点击发布
-//		//检查评论成功
-//				
-//		//返回首页
-//		Common.multiBack(driver, 2);
-//	}
-//	
-//	/**
-//	 * @function 我的评论列表-添加评论
-//	 * @throws Exception
-//	 */
-//	@Test(priority = 5)
-//	public void wode_mycommunity_testcase5() throws Exception{
-//		//清空评论
-//		WoDe.setCommentsEmpty(driver);
-//		//进入帮买车发表评论
-//		Common.touchText(driver, "帮买车");
-//		Common.touchText(driver, "最新");
-//		Common.touchText(driver, "topic btn action 2 nor");
-//		Common.sendText(driver, By.xpath("//UIAApplication[1]/UIAWindow[1]/UIATextView[1]"), "支持汽车大咖");
-//		Common.touchText(driver, "引用话题");//点击引用话题
-//		Common.touchText(driver,"nav right close");//关闭
-//		Common.touchText(driver, "发布");
-//		//返回，进入我的-我的社区-评论
-//		WoDe.enterCommentPage(driver);;//点击评论
-//		//检查评论发表成功
-//        Assert.assertTrue(Common.checkTextNotExist(driver, "您还没有发表过评论"),"检查评论发表成功");
-//        //返回首页
-//        Common.multiBack(driver, 2);
-//	}
-//	
-//	/**
-//	 * @function 我的评论列表-话题跳转
-//	 * @throws Exception
-//	 */
-//	@Test(priority = 6)
-//	public void wode_mycommunity_testcase6() throws Exception{
-//		//进入我的评论列表	
-//		WoDe.enterCommentPage(driver);
-//		//点击某一条收到的评论
-//		Common.touchXpath(driver, "//UIAApplication[1]/UIAWindow[1]/UIATableView[1]/UIATableCell[1]/UIAStaticText[1]");
-//		//检查进入话题详情页面
-//		Assert.assertTrue(Common.checkTextExist(driver, "话题详情"), "成功进入话题详情页");
-//		//返回首页
-//		Common.multiBack(driver, 3);	
-//	}
-//	
-//	/**
-//	 * @function 我的评论列表-删除
-//	 * @throws Exception
-//	 */
-//	@Test(priority = 7)
-//	public void wode_mycommunity_testcase7() throws Exception{
-//		//进入我的评论列表,删除评论列表
-//		WoDe.setCommentsEmpty(driver);
-//		//检查评论删除成功		
-//		Assert.assertTrue(Common.checkTextNotExist(driver, "删除"), "检查评论删除成功");
-//		//返回首页
-//		Common.multiBack(driver, 2);	
-//	}
-//	
-//	/**
-//	 * @function 我的关注列表-添加关注
-//	 * @throws Exception
-//	 */
-//	@Test(priority = 8)
-//	public void wode_mycommunity_testcase8() throws Exception{
-//		//清空关注列表
-//		WoDe.setFollowsEmpty(driver);
-//		//进入帮买车，添加关注
-//		BangMaiChe.addUserFollow(driver);
-//		//进入我的社区关注列表查看
-//		WoDe.enterFollowPage(driver);
-//		//检查关注成功
-//		Assert.assertTrue(Common.checkTextNotExist(driver, "您还没有关注任何人"), "检查添加关注成功！");
-//		//取消关注
-//		Common.touchXpath(driver,"//UIAApplication[1]/UIAWindow[1]/UIATableView[1]/UIATableCell[1]");
-//		Common.touchXpath(driver, "//UIAApplication[1]/UIAWindow[1]/UIATableView[1]/UIAButton[1]");
-//		Common.back(driver);
-//		//检查取消关注成功
-//		Assert.assertTrue(Common.checkTextNotExist(driver, "myAttention xhgz nor")&&Common.checkTextNotExist(driver, "myAttention ygz nor"),"检查取消关注成功！");//关注Text
-//		//返回首页
-//		Common.multiBack(driver, 2);
-//	}
-//	
-//	/**
-//	 * @function 我的关注列表-页面跳转
-//	 * @throws Exception
-//	 */
-//	@Test(priority = 9)
-//	public void wode_mycommunity_testcase9() throws Exception{
-//		//进入帮买车，添加关注
-//		BangMaiChe.addUserFollow(driver);
-//		//进入我的社区-关注
-//		WoDe.enterFollowPage(driver);
-//	    //点击关注列表
-//	    Common.touchXpath(driver, "//UIAApplication[1]/UIAWindow[1]/UIATableView[1]/UIATableCell[1]");
-//		// 检查成功进入他人主页
-//		Assert.assertTrue(Common.checkTextExist(driver, "btn yiguanzhu")||Common.checkTextExist(driver, "btn xhgz"), "检查成功进入他人主页！");
-//		//返回首页
-//		Common.multiBack(driver, 3);	
-//	}
-//	
-//	/**
-//	 * @function 我的粉丝列表-加关注
-//	 * @throws Exception
-//	 */
-//	@Test(priority = 10)
-//	public void wode_mycommunity_testcase10() throws Exception{
-//		//进入我的社区-粉丝			
-//		WoDe.enterFansPage(driver);
-//	    String text=Common.getText(driver, By.xpath("//UIAApplication[1]/UIAWindow[1]/UIATableView[1]/UIATableCell[1]/UIAButton[1]"));
-//	    //进行关注或取消关注
-//	    if(text.equals("myAttention guanzhu nor")){
-//	    	Common.touchText(driver,"myAttention guanzhu nor");
-//	    	Assert.assertTrue(Common.checkTextExist(driver, "myAttention xhgz nor"), "检查关注成功！");
-//	    }
-//	    else{
-//	    	Common.touchText(driver,"myAttention xhgz nor");
-//	    	Assert.assertTrue(Common.checkTextExist(driver, "myAttention guanzhu nor"), "检查取消关注成功！");
-//	    }
+	@Test(priority = 1)
+	public void wode_mycommunity_testcase1() throws Exception{
+		//清空我的话题列表
+		WoDe.setTopicEmpty(driver);
+		//发布话题
+		BangMaiChe.releaseGeneralTopic(driver);
+		//进入我的话题列表页
+		WoDe.enterTopicPage(driver);
+		//检查话题添加成功
+		Assert.assertTrue(Common.checkTextNotExist(driver,"您还没有发表过话题"),"检查话题发布成功");
+		//返回首页
+		Common.multiBack(driver, 2);
+	}
+	
+	/**
+	 * @function 我的话题列表-话题跳转
+	 * @precondition 接续testcase1
+	 * @throws Exception
+	 */
+	@Test(priority = 2)
+	public void wode_mycommunity_testcase2() throws Exception{
+		//进入我的-我的社区-话题
+		WoDe.enterTopicPage(driver);
+		//点击话题数据
+		Common.touchXpath(driver, "//UIAApplication[1]/UIAWindow[1]/UIATableView[1]/UIATableCell[1]");
+		//检查进入话题详情页
+		Assert.assertTrue(Common.checkTextExist(driver, "话题详情"), "成功进入话题详情页");
+		//返回首页
+		Common.multiBack(driver, 3);
+	}
+	
+	/**
+	 * @function 我的话题列表-点赞
+	 * @preconditon 接续testcase1
+	 * @throws Exception
+	 */
+	@Test(priority = 3)
+	public void wode_mycommunity_testcase3() throws Exception{
+		//进入我的-我的社区-话题
+		WoDe.enterTopicPage(driver);
+		//点赞
+		Common.touchXpath(driver, "//UIAApplication[1]/UIAWindow[1]/UIATableView[1]/UIATableCell[1]/UIAButton[3]");
+		//检查点赞成功
+		  
+		//返回首页
+		Common.multiBack(driver, 2);
+	}
+	
+	/**
+	 * @function 我的话题列表-评论
+	 * @precondion 接续testcase1
+	 * @throws Exception
+	 */
+	@Test(priority = 4)
+	public void wode_mycommunity_testcase4() throws Exception{
+		//进入我的-我的社区-话题
+		WoDe.enterTopicPage(driver);
+		//进行评论
+		Common.touchText(driver, "topic btn action 2 nor");//点击评论
+		Common.sendText(driver, By.xpath("//UIAApplication[1]/UIAWindow[1]/UIATextView[1]"), "嗯嗯");//输入内容
+		Common.touchText(driver, "引用话题");//点击引用话题
+		Common.touchText(driver,"nav right close");//关闭
+		Common.touchXpath(driver, "//UIAApplication[1]/UIAWindow[1]/UIAButton[4]");//点击发布
+		//检查评论成功
+				
+		//返回首页
+		Common.multiBack(driver, 2);
+	}
+	
+	/**
+	 * @function 我的评论列表-添加评论
+	 * @throws Exception
+	 */
+	@Test(priority = 5)
+	public void wode_mycommunity_testcase5() throws Exception{
+		//清空评论
+		WoDe.setCommentsEmpty(driver);
+		//进入帮买车发表评论
+		Common.touchText(driver, "帮买车");
+		Common.touchText(driver, "最新");
+		Common.touchText(driver, "topic btn action 2 nor");
+		Common.sendText(driver, By.xpath("//UIAApplication[1]/UIAWindow[1]/UIATextView[1]"), "支持汽车大咖");
+		Common.touchText(driver, "引用话题");//点击引用话题
+		Common.touchText(driver,"nav right close");//关闭
+		Common.touchText(driver, "发布");
+		//返回，进入我的-我的社区-评论
+		WoDe.enterCommentPage(driver);;//点击评论
+		//检查评论发表成功
+        Assert.assertTrue(Common.checkTextNotExist(driver, "您还没有发表过评论"),"检查评论发表成功");
+        //返回首页
+        Common.multiBack(driver, 2);
+	}
+	
+	/**
+	 * @function 我的评论列表-话题跳转
+	 * @throws Exception
+	 */
+	@Test(priority = 6)
+	public void wode_mycommunity_testcase6() throws Exception{
+		//进入我的评论列表	
+		WoDe.enterCommentPage(driver);
+		//点击某一条收到的评论
+		Common.touchXpath(driver, "//UIAApplication[1]/UIAWindow[1]/UIATableView[1]/UIATableCell[1]/UIAStaticText[1]");
+		//检查进入话题详情页面
+		Assert.assertTrue(Common.checkTextExist(driver, "话题详情"), "成功进入话题详情页");
+		//返回首页
+		Common.multiBack(driver, 3);	
+	}
+	
+	/**
+	 * @function 我的评论列表-删除
+	 * @throws Exception
+	 */
+	@Test(priority = 7)
+	public void wode_mycommunity_testcase7() throws Exception{
+		//进入我的评论列表,删除评论列表
+		WoDe.setCommentsEmpty(driver);
+		//检查评论删除成功		
+		Assert.assertTrue(Common.checkTextNotExist(driver, "删除"), "检查评论删除成功");
+		//返回首页
+		Common.multiBack(driver, 2);	
+	}
+	
+	/**
+	 * @function 我的关注列表-添加关注
+	 * @throws Exception
+	 */
+	@Test(priority = 8)
+	public void wode_mycommunity_testcase8() throws Exception{
+		//清空关注列表
+		WoDe.setFollowsEmpty(driver);
+		//进入帮买车，添加关注
+		BangMaiChe.addUserFollow(driver);
+		//进入我的社区关注列表查看
+		WoDe.enterFollowPage(driver);
+		//检查关注成功
+		Assert.assertTrue(Common.checkTextNotExist(driver, "您还没有关注任何人"), "检查添加关注成功！");
+		//取消关注
+		Common.touchXpath(driver,"//UIAApplication[1]/UIAWindow[1]/UIATableView[1]/UIATableCell[1]");
+		Common.touchXpath(driver, "//UIAApplication[1]/UIAWindow[1]/UIATableView[1]/UIAButton[1]");
+		Common.back(driver);
+		//检查取消关注成功
+		Assert.assertTrue(Common.checkTextNotExist(driver, "myAttention xhgz nor")&&Common.checkTextNotExist(driver, "myAttention ygz nor"),"检查取消关注成功！");//关注Text
+		//返回首页
+		Common.multiBack(driver, 2);
+	}
+	
+	/**
+	 * @function 我的关注列表-页面跳转
+	 * @throws Exception
+	 */
+	@Test(priority = 9)
+	public void wode_mycommunity_testcase9() throws Exception{
+		//进入帮买车，添加关注
+		BangMaiChe.addUserFollow(driver);
+		//进入我的社区-关注
+		WoDe.enterFollowPage(driver);
+	    //点击关注列表
+	    Common.touchXpath(driver, "//UIAApplication[1]/UIAWindow[1]/UIATableView[1]/UIATableCell[1]");
+		// 检查成功进入他人主页
+		Assert.assertTrue(Common.checkTextExist(driver, "btn yiguanzhu")||Common.checkTextExist(driver, "btn xhgz"), "检查成功进入他人主页！");
+		//返回首页
+		Common.multiBack(driver, 3);	
+	}
+	
+	/**
+	 * @function 我的粉丝列表-加关注
+	 * @throws Exception
+	 */
+	@Test(priority = 10)
+	public void wode_mycommunity_testcase10() throws Exception{
+		//进入我的社区-粉丝			
+		WoDe.enterFansPage(driver);
+	    String text=Common.getText(driver, By.xpath("//UIAApplication[1]/UIAWindow[1]/UIATableView[1]/UIATableCell[1]/UIAButton[1]"));
+	    //进行关注或取消关注
+	    if(text.equals("myAttention guanzhu nor")){
+	    	Common.touchText(driver,"myAttention guanzhu nor");
+	    	Assert.assertTrue(Common.checkTextExist(driver, "myAttention xhgz nor"), "检查关注成功！");
+	    }
+	    else{
+	    	Common.touchText(driver,"myAttention xhgz nor");
+	    	Assert.assertTrue(Common.checkTextExist(driver, "myAttention guanzhu nor"), "检查取消关注成功！");
+	    }
+	    //返回首页
+		Common.multiBack(driver, 2);	
+	}
+	
+	/**
+	 * @function 我的粉丝列表-页面跳转
+	 * @throws Exception
+	 */
+	@Test(priority = 11)
+	public void wode_mycommunity_testcase11() throws Exception{
+		//进入我的社区-粉丝			
+		WoDe.enterFansPage(driver);			
+		//点击某一粉丝
+		Common.touchXpath(driver, "//UIAApplication[1]/UIAWindow[1]/UIATableView[1]/UIATableCell[1]");
+		//检查成功进入他人主页		
+		Assert.assertTrue(WoDe.checkIsOtherHomepage(driver), "检查成功他人主页！");     
+		//返回首页
+		Common.multiBack(driver, 3);	
+	}
+	
+	/**
+	 * @function 收藏夹-添加收藏话题
+	 * @throws Exception
+	 */
+	@Test(priority = 12)
+	public void wode_mycommunity_testcase12() throws Exception{
+		//请空收藏夹数据
+		WoDe.setTopicCollectionEmpty(driver);
+		//添加收藏话题
+		BangMaiChe.addTopicCollection(driver);
+		//进入我的社区-收藏夹查看
+		WoDe.enterTopicCollectionPage(driver);
+		//检查收藏话题成功
+		Assert.assertTrue(Common.checkTextExist(driver, "topic btn action 6 nor"),"检查收藏话题成功！");
+		//返回首页
+		Common.multiBack(driver, 2);		
+	}
+	
+	/**
+	 * @function 收藏夹-页面跳转-他人主页
+	 * @precondition 接续testcase12 已有话题收藏
+	 * @throws Exception
+	 */
+	@Test(priority = 13)
+	public void wode_mycommunity_testcase13() throws Exception{
+		//进入我的社区-收藏夹					
+		WoDe.enterTopicCollectionPage(driver);
+		//页面跳转
+		Common.touchXpath(driver, "//UIAApplication[1]/UIAWindow[1]/UIATableView[1]/UIATableCell[1]/UIAButton[1]");
+		//检查进入成功进入他人主页
+		Assert.assertTrue(WoDe.checkIsOtherHomepage(driver), "检查成功进入他人主页！");
+		//返回首页
+		Common.multiBack(driver, 3);	
+	}
+	
+	/**
+	 * @function 收藏夹-页面跳转-话题详情页
+	 * @precondition 接续testcase12 已有话题收藏
+	 * @throws Exception
+	 */
+	@Test(priority = 14)
+	public void wode_mycommunity_testcase14() throws Exception{
+		//进入我的社区-收藏夹						
+		WoDe.enterTopicCollectionPage(driver);
+		//页面跳转
+		Common.touchXpath(driver, "//UIAApplication[1]/UIAWindow[1]/UIATableView[1]/UIATableCell[1]/UIAStaticText[1]");						
+		//检查进入成功进入话题详情页
+		Assert.assertTrue(Common.checkTextExist(driver, "话题详情"), "检查成功进入话题详情页！");
+		//返回
+		Common.multiBack(driver, 3);	
+	}
+	
+	/**
+	 * @function 收藏夹-点赞
+	 * @precondition 接续testcase12 已有话题收藏
+	 * @throws Exception
+	 */
+	@Test(priority = 15)
+	public void wode_mycommunity_testcase15() throws Exception{
+//		//进入我的社区-收藏夹						
+//		WoDe.enterTopicCollectionPage(driver);
+//	    //进行点赞
+//		Common.touchXpath(driver, "//UIAApplication[1]/UIAWindow[1]/UIATableView[1]/UIATableCell[1]/UIAButton[4]");//普通话题与投票话题xpath路径不一样	
+//	    //检查点赞成功
+//	    
 //	    //返回首页
 //		Common.multiBack(driver, 2);	
-//	}
-//	
-//	/**
-//	 * @function 我的粉丝列表-页面跳转
-//	 * @throws Exception
-//	 */
-//	@Test(priority = 11)
-//	public void wode_mycommunity_testcase11() throws Exception{
-//		//进入我的社区-粉丝			
-//		WoDe.enterFansPage(driver);			
-//		//点击某一粉丝
-//		Common.touchXpath(driver, "//UIAApplication[1]/UIAWindow[1]/UIATableView[1]/UIATableCell[1]");
-//		//检查成功进入他人主页		
-//		Assert.assertTrue(WoDe.checkIsOtherHomepage(driver), "检查成功他人主页！");     
-//		//返回首页
-//		Common.multiBack(driver, 3);	
-//	}
-//	
-//	/**
-//	 * @function 收藏夹-添加收藏话题
-//	 * @throws Exception
-//	 */
-//	@Test(priority = 12)
-//	public void wode_mycommunity_testcase12() throws Exception{
-//		//请空收藏夹数据
-//		WoDe.setTopicCollectionEmpty(driver);
-//		//添加收藏话题
-//		BangMaiChe.addTopicCollection(driver);
-//		//进入我的社区-收藏夹查看
-//		WoDe.enterTopicCollectionPage(driver);
-//		//检查收藏话题成功
-//		Assert.assertTrue(Common.checkTextExist(driver, "topic btn action 6 nor"),"检查收藏话题成功！");
-//		//返回首页
-//		Common.multiBack(driver, 2);		
-//	}
-//	
-//	/**
-//	 * @function 收藏夹-页面跳转-他人主页
-//	 * @precondition 接续testcase12 已有话题收藏
-//	 * @throws Exception
-//	 */
-//	@Test(priority = 13)
-//	public void wode_mycommunity_testcase13() throws Exception{
-//		//进入我的社区-收藏夹					
-//		WoDe.enterTopicCollectionPage(driver);
-//		//页面跳转
-//		Common.touchXpath(driver, "//UIAApplication[1]/UIAWindow[1]/UIATableView[1]/UIATableCell[1]/UIAButton[1]");
-//		//检查进入成功进入他人主页
-//		Assert.assertTrue(WoDe.checkIsOtherHomepage(driver), "检查成功进入他人主页！");
-//		//返回首页
-//		Common.multiBack(driver, 3);	
-//	}
-//	
-//	/**
-//	 * @function 收藏夹-页面跳转-话题详情页
-//	 * @precondition 接续testcase12 已有话题收藏
-//	 * @throws Exception
-//	 */
-//	@Test(priority = 14)
-//	public void wode_mycommunity_testcase14() throws Exception{
-//		//进入我的社区-收藏夹						
-//		WoDe.enterTopicCollectionPage(driver);
-//		//页面跳转
-//		Common.touchXpath(driver, "//UIAApplication[1]/UIAWindow[1]/UIATableView[1]/UIATableCell[1]/UIAStaticText[1]");						
-//		//检查进入成功进入话题详情页
-//		Assert.assertTrue(Common.checkTextExist(driver, "话题详情"), "检查成功进入话题详情页！");
-//		//返回
-//		Common.multiBack(driver, 3);	
-//	}
-//	
-//	/**
-//	 * @function 收藏夹-点赞
-//	 * @precondition 接续testcase12 已有话题收藏
-//	 * @throws Exception
-//	 */
-//	@Test(priority = 15)
-//	public void wode_mycommunity_testcase15() throws Exception{
-////		//进入我的社区-收藏夹						
-////		WoDe.enterTopicCollectionPage(driver);
-////	    //进行点赞
-////		Common.touchXpath(driver, "//UIAApplication[1]/UIAWindow[1]/UIATableView[1]/UIATableCell[1]/UIAButton[4]");//普通话题与投票话题xpath路径不一样	
-////	    //检查点赞成功
-////	    
-////	    //返回首页
-////		Common.multiBack(driver, 2);	
-//	}
-//		
-//	/**
-//	 * @function 收藏夹-评论
-//	 * @precondition 接续testcase12 已有话题收藏
-//	 * @throws Exception
-//	 */
-//	@Test(priority = 16)
-//	public void wode_mycommunity_testcase16() throws Exception{
-//		//进入我的社区-收藏夹						
-//		WoDe.enterTopicCollectionPage(driver);
-//		//进行评论
-//		Common.touchText(driver, "topic btn action 2 nor");
-//		Common.sendText(driver, By.xpath("//UIAApplication[1]/UIAWindow[1]/UIATextView[1]"), "打酱油");
-//		Common.touchText(driver, "引用话题");
-//		Common.touchText(driver, "nav right close");
-//		Common.touchText(driver, "发布");
-//		//检查评论成功
-//		
-//		//返回首页
-//		Common.multiBack(driver, 2);	
-//	}
-//		
-//	
-//	/**
-//	 * @function 收藏夹列表-取消收藏
-//	 * @precondition 接续testcase12 已有话题收藏
-//	 * @throws Exception
-//	 */
-//	@Test(priority = 17)
-//	public void wode_mycommunity_testcase17() throws Exception{
-//		//进入我的社区-收藏夹						
-//		WoDe.enterTopicCollectionPage(driver);
-//		//取消收藏
-//		Common.touchText(driver, "topic btn action 6 nor");
-//		//检查取消收藏成功
-//		Assert.assertTrue(Common.checkTextExist(driver, "topic btn action 7 nor"), "检查取消收藏成功！");
-//		//返回首页
-//		Common.multiBack(driver, 2);	
-//	}
+	}
+		
+	/**
+	 * @function 收藏夹-评论
+	 * @precondition 接续testcase12 已有话题收藏
+	 * @throws Exception
+	 */
+	@Test(priority = 16)
+	public void wode_mycommunity_testcase16() throws Exception{
+		//进入我的社区-收藏夹						
+		WoDe.enterTopicCollectionPage(driver);
+		//进行评论
+		Common.touchText(driver, "topic btn action 2 nor");
+		Common.sendText(driver, By.xpath("//UIAApplication[1]/UIAWindow[1]/UIATextView[1]"), "打酱油");
+		Common.touchText(driver, "引用话题");
+		Common.touchText(driver, "nav right close");
+		Common.touchText(driver, "发布");
+		//检查评论成功
+		
+		//返回首页
+		Common.multiBack(driver, 2);	
+	}
+		
+	
+	/**
+	 * @function 收藏夹列表-取消收藏
+	 * @precondition 接续testcase12 已有话题收藏
+	 * @throws Exception
+	 */
+	@Test(priority = 17)
+	public void wode_mycommunity_testcase17() throws Exception{
+		//进入我的社区-收藏夹						
+		WoDe.enterTopicCollectionPage(driver);
+		//取消收藏
+		Common.touchText(driver, "topic btn action 6 nor");
+		//检查取消收藏成功
+		Assert.assertTrue(Common.checkTextExist(driver, "topic btn action 7 nor"), "检查取消收藏成功！");
+		//返回首页
+		Common.multiBack(driver, 2);	
+	}
 	
 	/**
 	 * @function 草稿箱-添加数据
