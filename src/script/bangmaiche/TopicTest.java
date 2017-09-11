7bdca2395f4ae57af720ef84dc7e810e7318e198
xulishuang <xulishuang_bit@126.com>
2017-09-11 17:35:52 +0800

    7.8

--- src/script/bangmaiche/TopicTest.java
+++ src/script/bangmaiche/TopicTest.java
@@ -17,6 +17,8 @@ public class TopicTest extends AppTestSuite {
 	 */	
   @Test(priority = 1)
   public void bangmaiche_topic_testcase1()  {
+	  //清空我的社区-话题
+	  WoDe.setTopicEmpty(driver);
 	  //进入最新页面
 	  Common.touchText(driver, "帮买车");
 	  Common.touchText(driver,"最新");
@@ -54,13 +56,16 @@ public class TopicTest extends AppTestSuite {
 	 */	
   @Test(priority = 2)
   public void bangmaiche_topic_testcase2(){
+	  //进入帮买车-罪行
+	  Common.touchText(driver, "帮买车");
+	  Common.touchText(driver, "最新");
 	  //进入testcase1中已发的普通话题
 	  Common.touchSlideText(driver, Text.ACCOUNT, 5);
 	  //删除testcase1中已发布的评论
-	  Common.touchText(driver, "删除");
+	  Common.touchXpath(driver, "//UIAApplication[1]/UIAWindow[1]/UIATableView[1]/UIATableCell[4]/UIAButton[3]");
 	  Common.touchText(driver,"确定");
 	  //检查删除成功
-	  Assert.assertTrue(Common.checkTextNotExist(driver,"看一看"), "检查评论删除成功");
+	  
 	  //返回首页
 	  Common.back(driver);
   }
@@ -75,13 +80,13 @@ public class TopicTest extends AppTestSuite {
 	  //进入testcase1中已发的普通话题
 	  Common.touchSlideText(driver, Text.ACCOUNT, 5);
 	  //点赞
-	  Common.touchText(driver, "//UIAApplication[1]/UIAWindow[1]/UIATableView[1]/UIATableCell[3]/UIAButton[3]");
+	  Common.touchXpath(driver, "//UIAApplication[1]/UIAWindow[1]/UIATableView[1]/UIATableCell[3]/UIAButton[3]");
 	  //进入点赞用户列表，检查点赞成功
 	  Common.touchXpath(driver, "//UIAApplication[1]/UIAWindow[1]/UIATableView[1]/UIATableCell[3]/UIAStaticText[2]");
 	  Assert.assertTrue(Common.checkTextExist(driver, Text.ACCOUNT), "检查点赞成功");
 	  Common.back(driver);
 	  //再次点赞
-	  Common.touchText(driver, "//UIAApplication[1]/UIAWindow[1]/UIATableView[1]/UIATableCell[3]/UIAButton[3]");
+	  Common.touchXpath(driver, "//UIAApplication[1]/UIAWindow[1]/UIATableView[1]/UIATableCell[3]/UIAButton[3]");
 	  //检查取消点赞
 	  if(Common.checkXpathExist(driver, "//UIAApplication[1]/UIAWindow[1]/UIATableView[1]/UIATableCell[3]/UIAStaticText[2]")){
 		  Common.touchXpath(driver, "//UIAApplication[1]/UIAWindow[1]/UIATableView[1]/UIATableCell[3]/UIAStaticText[2]");
@@ -103,13 +108,18 @@ public class TopicTest extends AppTestSuite {
 	  //点击底部文字输入框
 	  Common.touchText(driver,"  说点什么吧....");
 	  //输入文字
-	  Common.sendText(driver, By.xpath("//UIAApplication[1]/UIAWindow[1]/UIATextView[1]"), "看一看");
+	  Common.sendText(driver, By.xpath("//UIAApplication[1]/UIAWindow[1]/UIATextView[1]"), "点赞");
+	  //点击关联车型
+	  Common.touchText(driver,"+车型");
+	  //选择车型
+	  Common.touchText(driver,"奥迪");
+	  Common.touchText(driver, "奥迪Q5");
 	  //点击发布
 	  Common.touchText(driver, "发布");
 	  //点击评论回复
 	  Common.touchText(driver,"topic btn comment nor");
 	  //输入文字
-	  Common.sendText(driver, By.xpath("/UIAApplication[1]/UIAWindow[1]/UIATextView[1]"), "嘿嘿");
+	  Common.sendText(driver, By.xpath("//UIAApplication[1]/UIAWindow[1]/UIATextView[1]"), "嘿嘿");
 	  //点击关联车型
 	  Common.touchText(driver,"+车型");
 	  //选择车型
@@ -172,7 +182,7 @@ public class TopicTest extends AppTestSuite {
 	  Common.touchText(driver,"我的");
 	  Common.touchText(driver, "我的社区");
 	  Common.touchText(driver,"收藏夹");
-	  Assert.assertFalse(Common.checkTextExist(driver,"没有任何收藏"), "检查收藏成功");
+	  Assert.assertTrue(Common.checkTextExist(driver,"topic btn action 6 nor"),"检查收藏成功");
 	  //返回首页
 	  Common.multiBack(driver, 2);
   }
